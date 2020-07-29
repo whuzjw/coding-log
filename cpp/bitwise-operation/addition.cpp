3dbecdd8d5a8732f7cbbcc47c5070dbb4aa012b7
whuzjw <169765778@qq.com>
2020-07-29 12:39:55 +0800

    add a dictionary bitwise-operation

---
+++ cpp/bitwise-operation/addition.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	int sum = a ^ b;
+	int carry = (a & b) << 1;
+	while (carry != 0) {
+		int temp = sum;
+		sum ^= carry;
+		carry = (temp & carry) << 1;
+	}
+	cout << sum<<endl;
+	return 0;
+}
